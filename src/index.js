c5b0b28909dfc35a275d93dd0cbe70102a6ede97
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-13 11:24:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 const { JsonDatabase } = require("../src/JsonDatabase")
-const { TopGG } = require("../src/Topgg")
-const { Color } = require("../src/Color")
-const { BaskanAI } = require("../src/BaskanAI")
-module.exports = { JsonDatabase, TopGG, Color, BaskanAI }
+module.exports = { JsonDatabase }
