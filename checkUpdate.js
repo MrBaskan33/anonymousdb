1ad6551089c0ea30a18b1007e838b7de5b9641cc
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-14 11:26:21 +0300

    Update checkUpdate.js

--- checkUpdate.js
+++ checkUpdate.js
@@ -4,13 +4,13 @@ global.hasCheckedForUpdates = global.hasCheckedForUpdates || false
 
 async function checkUpdates() {
   try {
-    const data = await axios({ method: "GET", url: "https://registry.npmjs.org/baskan-db/latest" }).then(res => res.data.version)
+    const data = await axios({ method: "GET", url: "https://registry.npmjs.org/anonymousdb/latest" }).then(res => res.data.version)
     if(packageJson.version !== data) {
-      return console.error("Your baskan-db version is outdated.\nUpdate it by running 'npm i baskan-db@latest'")
+      return console.error("Your anonymousdb version is outdated.\nUpdate it by running 'npm i anonymousdb@latest'")
     } 
   } catch (error) {
     if(err.message == "Request failed with status code 429") {
-      return console.error("Too many requests to check the latest version of baskan-.db. Please try again later.")
+      return console.error("Too many requests to check the latest version of anonymousdb. Please try again later.")
     }
     return console.error("Error occurred while checking for updates:", error)
   }
